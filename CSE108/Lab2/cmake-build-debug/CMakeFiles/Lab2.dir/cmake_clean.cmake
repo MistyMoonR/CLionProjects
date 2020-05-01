8c7844d6a3a4aaa3f9babed819989f43d1dd0c6f
MistyMoon <mistymoonr@outlook.com>
2020-05-01 20:10:30 +0800

    add lab2

---
+++ CSE108/Lab2/cmake-build-debug/CMakeFiles/Lab2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2.dir/main.c.obj"
+  "Lab2.exe"
+  "Lab2.exe.manifest"
+  "Lab2.pdb"
+  "libLab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
